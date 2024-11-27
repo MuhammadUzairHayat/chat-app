aa1d69936209a00bbc3cf21281aba0d9297e83da
Muhammad Uzair <muhammad.druzair@gmail.com>
2024-11-27 17:36:12 +0500

    chat-app  chatBox

--- src/Pages/Chat/Chat.jsx
+++ src/Pages/Chat/Chat.jsx
@@ -1,16 +1,17 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
+import './Chat.css'
 import ChatBox from '../../components/ChatBox/ChatBox'
 import RightSidebar from '../../components/RightSidebar/RightSidebar'
 import LiftSidebar from '../../components/LiftSidebar/LiftSidebar'
 
 const Chat = () => {
   return (
-    <div>
+    <div className='chat'>
       <div className="chat-container">
-        <RightSidebar />
-        <ChatBox />
         <LiftSidebar />
+        <ChatBox />
+        <RightSidebar />
       </div>
     </div>
   )
--- src/components/LiftSidebar/LiftSidebar.jsx
+++ src/components/LiftSidebar/LiftSidebar.jsx
@@ -1,12 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from "react";
+import "./LiftSidebar.css";
+import assets from "../../assets/assets";
 
 const LiftSidebar = () => {
   return (
     <div>
-      
+      <div className="ls">
+        <div className="ls-header">
+          <div className="ls-nav">
+            <img className="ls-logo" src={assets.logo} alt="" />
+            <img className="ls-menu-icon" src={assets.menu_icon} alt="" />
+          </div>
+          <div className="ls-search">
+            <img className="ls-search-icon" src={assets.search_icon} alt="" />
+            <input type="text" placeholder="Search..." />
+          </div>
+        </div>
+        <div className="ls-main">
+          {Array(15)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="ls-each-profile">
+                <img
+                  className="ls-profile-img"
+                  src={assets.profile_img}
+                  alt=""
+                />
+                <div className="ls-profile-info">
+                  <h4>John Doe {index}</h4>
+                  <p>Software Engineer</p>
+                </div>
+                {/* Uncomment the following if needed */}
+                {/* <div className="ls-profile-status">
+        <div className="ls-green-dot"></div>
+        <p>Active</p>
+      </div> */}
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LiftSidebar
+export default LiftSidebar;
