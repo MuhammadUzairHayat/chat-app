dece04f3acd127709543c2c60979acde272f26c4
Muhammad Uzair <muhammad.druzair@gmail.com>
2024-11-28 09:48:41 +0500

    fresh chatBox

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -22,16 +22,22 @@ const ChatBox = () => {
       </div>
 
       <div className="chat-message">
-        <div className="chat-message-div">
+        <div className="chat-message-s">
           <div className="chat-message-content">
-            <p>Hello, how are you today? </p>
-            <span className="chat-message-time">12:30 PM</span>
+            <div className="chat-avatar-time">
+              <img src={assets.profile_img} alt="" />
+              <span className="chat-time">12:31 PM</span>
+            </div>
+            <p>I am doing well, thank you! How about you? </p>
           </div>
         </div>
-        <div className="chat-message-div">
+        <div className="chat-message-r">
           <div className="chat-message-content">
-            <p>I am doing well, thank you! How about you? </p>
-            <span className="chat-message-time">12:35 PM</span>
+            <div className="chat-avatar-time">
+              <img src={assets.profile_img} alt="" />
+              <span className="chat-time">12:31 PM</span>
+            </div>
+            <p>I am doing well, thank you! How about you?  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad minima ratione ducimus reprehenderit expedita libero. Doloribus quibusdam sit laudantium necessitatibus?</p>
           </div>
         </div>
       </div>
