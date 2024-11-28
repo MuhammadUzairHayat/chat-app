64352edadb8e6b6994d40625c3a0e4317e0cc64b
Muhammad Uzair <muhammad.druzair@gmail.com>
2024-11-28 12:44:37 +0500

    updated chatBox new

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -1,26 +1,34 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import './ChatBox.css';
-import assets from '../../assets/assets';
-
+import React from "react";
+import "./ChatBox.css";
+import assets from "../../assets/assets";
 
 const ChatBox = () => {
   return (
-    <div className='chat-box'>
+    <div className="chat-box">
+      {/* ---- Chat_Top_User_Bio -----*/}
       <div className="chat-user">
         <div className="chat-user-intro">
           <img src={assets.profile_img} alt="" />
-          <span className='user'>
-            <span className='chat-name-dot'> 
+          <span className="user">
+            <span className="chat-name-dot">
               <span className="chat-user-name">User Name </span>
               <div className="active-dot"></div>
             </span>
             <p className="chat-user-status">online </p>
           </span>
         </div>
-        <img style={{width: '20px', height: '20px'}} className='chat-info-icon' src={assets.info_icon} alt="" />
+        <img
+          style={{ width: "20px", height: "20px" }}
+          className="chat-info-icon"
+          src={assets.info_icon}
+          alt=""
+        />
       </div>
 
+      {/* ---- Chatting-Of-User ----- */}
+
+      {/* --- Sender_Messages ---- */}
       <div className="chat-message">
         <div className="chat-message-s">
           <div className="chat-message-content">
@@ -28,28 +36,57 @@ const ChatBox = () => {
               <img src={assets.profile_img} alt="" />
               <span className="chat-time">12:31 PM</span>
             </div>
-            <p>I am doing well, thank you! How about you? </p>
+            <p className="msg">I am doing well, thank you! How about you? </p>
           </div>
         </div>
+
+        {/* --- Sender_Images ---- */}
+          <div className="chat-message-s">
+            <div className="chat-message-content">
+              <div className="chat-avatar-time">
+                <img src={assets.profile_img} alt="" />
+                <span className="chat-time">12:31 PM</span>
+              </div>{" "}
+              <img className="send-img" src={assets.pic1} alt="" />
+            </div>
+          </div>
+
+          {/* --- Receiver_Messages ---- */}
+          <div className="chat-message-r">
+            <div className="chat-message-content">
+              <div className="chat-avatar-time">
+                <img src={assets.profile_img} alt="" />
+                <span className="chat-time">12:31 PM</span>
+              </div>
+              <p className="msg">
+                I am doing well, thank you! How about you? Lorem ipsum dolor sit
+                amet, consectetur adipisicing elit. Ad minima ratione ducimus
+                reprehenderit expedita libero. Doloribus quibusdam sit
+                laudantium necessitatibus?
+              </p>
+            </div>
+          </div>
+
+        {/* --- Receiver_Images ---- */}
         <div className="chat-message-r">
           <div className="chat-message-content">
             <div className="chat-avatar-time">
               <img src={assets.profile_img} alt="" />
               <span className="chat-time">12:31 PM</span>
             </div>
-            <p>I am doing well, thank you! How about you?  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad minima ratione ducimus reprehenderit expedita libero. Doloribus quibusdam sit laudantium necessitatibus?</p>
+            <img className="receive-img" src={assets.pic2} alt="" />
           </div>
         </div>
       </div>
 
+      {/* ----- Send_Message_Input ------ */}
       <div className="chat-send-input">
-        <input type="text" placeholder='Type a message...' />
-        <img className='chat-gallery-icon' src={assets.gallery_icon} alt="" />
-        <img className='chat-send-icon' src={assets.send_button} alt="" />
-
+        <input type="text" placeholder="Type a message..." />
+        <img className="chat-gallery-icon" src={assets.gallery_icon} alt="" />
+        <img className="chat-send-icon" src={assets.send_button} alt="" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatBox
+export default ChatBox;
